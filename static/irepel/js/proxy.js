62155fd5501687c9969633bf68dc224f908f977e
0xkkl <kaydan@leozc.com>
2023-04-14 19:24:00 -0700

    ADDED IREPEL OMGGG THIS TOOK SO LONG AHHH

---
+++ static/irepel/js/proxy.js
@@ -0,0 +1,32 @@
+function proxy(url) {
+	worker().then(e=>{
+		if (!url.startsWith('http')) url = 'https://' + url;
+	
+		location.assign(window.__uv$config.prefix + window.__uv$config.encodeUrl(url));
+	});
+}
+
+async function worker() {
+	var a = await navigator.serviceWorker.register('/uv.sw-handler.js', {scope: '/service'});
+	return a;
+}
+
+if (window.location.pathname === '/irepel/proxy.html') {
+	console.log('proxy.html');
+	document.querySelector('.uvform').addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		worker().then(e=>{
+			var val = document.querySelector('.uvinput').value;
+			if (!val.startsWith('http')) val = 'https://' + val;
+		
+			location.assign(window.__uv$config.prefix + window.__uv$config.encodeUrl(val));
+		});
+	});
+	const queryParams = new URLSearchParams(window.location.search);
+	const url = queryParams.get('url');
+
+	if (url) {
+		proxy(url);
+	}
+}
