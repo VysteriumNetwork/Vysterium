bb32a744fcddf65b0bc17ecd7ef8ab2c01435473
0xkkl <kaydan@leozc.com>
2023-04-15 07:24:18 -0700

    added cosmic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import createBareServer from '@tomphttp/bare-server-node';
-import { fileURLToPath } from "url";
+import createBareServer from "@tomphttp/bare-server-node";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+
+import { fileURLToPath } from "node:url";
 import { createServer as createHttpsServer } from "node:https";
 import { createServer as createHttpServer } from "node:http";
 import { readFileSync, existsSync } from "node:fs";
 import { hostname } from "node:os";
+
 import serveStatic from "serve-static";
+import connect from "connect";
+const app = connect();
 const bare = createBareServer("/bare/");
-const serve = serveStatic(fileURLToPath(new URL("../static/", import.meta.url)), { fallthrough: false });
 var server, PORT = process.env.PORT;
 const ssl = existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem");
 if(ssl) {
@@ -14,34 +18,39 @@ if(ssl) {
     key: readFileSync("../ssl/key.pem"),
     cert: readFileSync("../ssl/cert.pem")
   });
-  PORT = (PORT || 80);
+  PORT = (PORT || 443);
 } else { server = createHttpServer(); PORT = (PORT || 80);}
 
-server.on("request", (req, res) => {
-  if(bare.shouldRoute(req)) return bare.routeRequest(req, res);
-    serve(req, res, (err) => {
-      res.writeHead(err?.statusCode || 500, null, {
-        "Content-Type": "text/plain",
-      });
-      res.end('Error')
-    })
+app.use((req, res, next) => {
+  if(bare.shouldRoute(req)) bare.routeRequest(req, res); else next();
 });
 
+app.use(serveStatic(fileURLToPath(new URL("../static/", import.meta.url))));
+
+app.use("/uv", serveStatic(uvPath));
+
+app.use((req, res) => {
+  res.writeHead(500, null, {
+    "Content-Type": "text/plain",
+  });
+  res.end("Error");
+});
+
+server.on("request", app);
 server.on("upgrade", (req, socket, head) => {
   if(bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head); else socket.end();
 });
 
-server.on('listening', () => {
+server.on("listening", () => {
   const addr = server.address();
 
   console.log(`Server running on port ${addr.port}`)
-  console.log('');
-  console.log('You can now view it in your browser.')
+  console.log("");
+  console.log("You can now view it in your browser.")
   /* Code for listing IPS from website-aio */
-  console.log(`Local: ${ssl ? 'https' : 'http'}://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}${(addr.port === 80 || ssl && addr.port === 443)? '' : ':' + addr.port}`);
-  console.log(`Local: ${ssl ? 'https' : 'http'}://localhost${(addr.port === 80 || ssl && addr.port === 443)? '' : ':' + addr.port}`);
-  try { console.log(`On Your Network: ${ssl ? 'https' : 'http'}://${hostname()}${(addr.port === 80 || ssl && addr.port === 443)? '' : ':' + addr.port}`); } catch (err) {/* Can't find LAN interface */};
-  if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
+  console.log(`Local: http${ssl ? "s" : ""}://${addr.family === "IPv6" ? `[${addr.address}]` : addr.address}${(addr.port === 80 || ssl && addr.port === 443)? "" : ":" + addr.port}`);
+  console.log(`Local: http${ssl ? "s" : ""}://localhost${(addr.port === 80 || ssl && addr.port === 443)? "" : ":" + addr.port}`);
+  try { console.log(`On Your Network: http${ssl ? "s" : ""}://${hostname()}${(addr.port === 80 || ssl && addr.port === 443)? "" : ":" + addr.port}`); } catch (err) {/* Can't find LAN interface */};
 });
 
 server.listen({ port: PORT })
