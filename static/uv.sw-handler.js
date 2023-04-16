04990678de8a58272a43489990c4cd04e3230cd5
0xkkl <kaydan@leozc.com>
2023-04-16 09:30:10 -0700

    added neb (pain)

--- static/uv.sw-handler.js
+++ static/uv.sw-handler.js
@@ -1,9 +1,15 @@
 importScripts('/uv/uv.bundle.js');
 importScripts('/uv/uv.config.js');
 importScripts(__uv$config.sw || '/uv/uv.sw.js');
-
+importScripts("/osana/osana.worker.js");
 const sw = new UVServiceWorker();
+const Osana = new OsanaServiceWorker();
 
-self.addEventListener('fetch', event => {
-    event.respondWith(sw.fetch(event))
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.startsWith(location.origin + "/service/")) {
+  event.respondWith(sw.fetch(event))
+  }
+  if (event.request.url.startsWith(location.origin + "osana/")) {
+    event.respondWith(Osana.fetch(event));
+  }
 });
