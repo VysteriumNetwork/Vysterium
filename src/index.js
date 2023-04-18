5b0a56e8bb19b25444dce134a8d5324dd3104fb7
urasdbwd <102772348+urasdbwd@users.noreply.github.com>
2023-04-18 15:10:20 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 import createBareServer from "@tomphttp/bare-server-node";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+
 import { fileURLToPath } from "node:url";
 import { createServer as createHttpsServer } from "node:https";
 import { createServer as createHttpServer } from "node:http";
 import { readFileSync, existsSync } from "node:fs";
 import { hostname } from "node:os";
-import connect from "connect";
+
 import serveStatic from "serve-static";
+import connect from "connect";
 const app = connect();
 const bare = createBareServer("/bare/");
 var server, PORT = process.env.PORT;
 const ssl = existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem");
-if(ssl) {
+if (ssl) {
   server = createHttpsServer({
     key: readFileSync("../ssl/key.pem"),
     cert: readFileSync("../ssl/cert.pem")
   });
   PORT = (PORT || 443);
-} else { server = createHttpServer(); PORT = (PORT || 8080);}
+} else {
+  server = createHttpServer();
+  PORT = (PORT || 80);
+}
 
 app.use((req, res, next) => {
-  if(bare.shouldRoute(req)) bare.routeRequest(req, res); else next();
+  if (bare.shouldRoute(req)) bare.routeRequest(req, res); else next();
 });
-app.use(serveStatic(fileURLToPath(new URL("../static/", import.meta.url))));
-
-app.use("/uv", serveStatic(uvPath));
 
 app.use((req, res, next) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
@@ -32,9 +34,14 @@ app.use((req, res, next) => {
   if (isLS) {
     app.use(serveStatic(fileURLToPath(new URL("../BlacklistServe/", import.meta.url))));
   } else {
-    app.use(serveStatic(fileURLToPath(new URL("../static/", import.meta.url))));
+    next();
   }
-  next();
+});
+
+app.use(serveStatic(fileURLToPath(new URL("../static/", import.meta.url))));
+app.use("/uv", serveStatic(uvPath));
+
+app.use((req, res) => {
   res.writeHead(500, null, {
     "Content-Type": "text/plain",
   });
@@ -43,7 +50,7 @@ app.use((req, res, next) => {
 
 server.on("request", app);
 server.on("upgrade", (req, socket, head) => {
-  if(bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head); else socket.end();
+  if (bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head); else socket.end();
 });
 
 server.on("listening", () => {
@@ -58,4 +65,4 @@ server.on("listening", () => {
   try { console.log(`On Your Network: http${ssl ? "s" : ""}://${hostname()}${(addr.port === 80 || ssl && addr.port === 443)? "" : ":" + addr.port}`); } catch (err) {/* Can't find LAN interface */};
 });
 
-server.listen({ port: PORT })
+server.listen({ port: PORT });
