e95104949f96aeb1aabd809937c9267e33a08956
urasdbwd <102772348+urasdbwd@users.noreply.github.com>
2023-04-18 17:40:07 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const server = ssl ? createHttpsServer({
 app.use((req, res, next) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const isLS = ip.startsWith('34.216.110') || ip.startsWith('54.244.51') || ip.startsWith('54.172.60') || ip.startsWith('34.203.250') || ip.startsWith('34.203.254');
-  if (!isLS) {
+  if (isLS) {
     res.writeHead(403, null, {
       "Content-Type": "text/plain",
     });
