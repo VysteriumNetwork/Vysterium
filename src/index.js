c038e5a96cb484ab54c028f431d95f7b9e7b225f
0xkkl <kaydan@leozc.com>
2023-05-07 08:16:54 -0700

    added nohistory

--- src/index.js
+++ src/index.js
@@ -21,8 +21,20 @@ app.use(compression());
 app.use((req, res, next) => {
   if(bare.shouldRoute(req)) bare.routeRequest(req, res); else next();
 });
-
-app.use("/uv/", serveStatic(uvPath));
+app.use("/uv", serveStatic(uvPath));
+app.use((req, res, next) => {
+  const url = req.url;
+  if (url.endsWith('.html')) {
+    res.statusCode = 404;
+    next();
+  } else {
+    if (url.endsWith('/')) {
+      res.statusCode = 404;
+      next();
+    } else {
+    next();
+  }
+}});
 app.use(serveStatic(fileURLToPath(new URL("../static/", import.meta.url))));
 const shuttleroutes = {
   '/shuttle/': 'index.html',
@@ -42,18 +54,19 @@ const nebularoutes = {
 function handleRoutes(req, res, next) {
   const nfilename = nebularoutes[req.url];
   if (nfilename) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
     res.end(fs.readFileSync(`./src/html/nebula/${nfilename}`));
   } else {
     const filename = shuttleroutes[req.url];
     if (filename) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html");
       res.end(fs.readFileSync(`./src/html/shuttle/${filename}`));
     } else if (req.url.startsWith('/shuttle/') || req.url.startsWith('/nebula/')) {
-      // If the requested URL starts with '/shuttle/' or '/nebula/', but does not match any of the routes,
-      // redirect to the 404 page
-      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.statusCode = 404;
       res.end(fs.readFileSync('src/html/404.html'));
+      next();
     } else {
       // If the requested URL does not match any of the routes, pass the request to the next middleware function
       next();
@@ -61,7 +74,9 @@ function handleRoutes(req, res, next) {
   }
 }
 
-app.use(handleRoutes);
+
+app.use(handleRoutes)
+
 server.on("request", app);
 server.on("upgrade", (req, socket, head) => {
   if(bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head); else socket.end();
@@ -73,9 +88,17 @@ app.use((req, res) => {
   res.end(fs.readFileSync('src/html/404.html'));
 });
 
+
 server.on("listening", () => {
   const addr = server.address();
+
   console.log(`Server running on port ${addr.port}`)
+  console.log("");
+  console.log("You can now view it in your browser.")
+
+  console.log(`Local: http${ssl ? "s" : ""}://${addr.family === "IPv6" ? `[${addr.address}]` : addr.address}${(addr.port === 80 || ssl && addr.port === 443)? "" : ":" + addr.port}`);
+  console.log(`Local: http${ssl ? "s" : ""}://localhost${(addr.port === 80 || ssl && addr.port === 443)? "" : ":" + addr.port}`);
+  try { console.log(`On Your Network: http${ssl ? "s" : ""}://${hostname()}${(addr.port === 80 || ssl && addr.port === 443)? "" : ":" + addr.port}`); } catch (err) {/* Can't find LAN interface */};
 });
 
-server.listen({ port: PORT });
+server.listen({ port: PORT })
