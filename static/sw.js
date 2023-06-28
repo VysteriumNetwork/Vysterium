0ea20b692fe653bceed25b62efef2b29b65e36ff
0xkkl <kaydan@leozc.com>
2023-06-27 21:45:45 -0700

    changed useragent

--- static/sw.js
+++ static/sw.js
@@ -6,7 +6,7 @@ let sw = new ServiceWorker();
 let bareReady = fetchAndSetBare();
 sw.on("request", (event) => {
   event.data.headers["user-agent"] =
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_4_8; en-US) AppleWebKit/601.14 (KHTML, like Gecko) Chrome/52.0.3548.230 Safari/534";
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0";
 });
 self.addEventListener('fetch', event => {
   event.respondWith(
