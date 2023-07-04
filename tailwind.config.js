f9b6477dc6273438efc2360f2b3d04b4f27311ef
0xkkl <kaydan@leozc.com>
2023-07-03 20:35:58 -0700

    added tailwind stuff

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    content: {
+      relative: true,
+      files: ['./static/index.html', './static/elixir/index.html', './static/elixir/about.html', './static/settings.html'],
+    },
+    theme: {
+      extend: {
+        darkMode: true,
+        fontFamily: {
+          'Inter': ['Inter', 'sans-serif']
+        },
+        colors: {
+          'primary': '#0d2c4c',
+          'secondary': '#d36135',
+          'jet-black': '#0c0c0c',
+          'ghost-white': '#f4f4f9'
+        }
+      },
+    },
+    plugins: [
+    ],
+  }
