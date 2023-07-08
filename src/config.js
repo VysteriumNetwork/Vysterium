1f1c4650faa982096dd24a95829f0a679560d137
0xkkl <kaydan@leozc.com>
2023-07-07 21:25:59 -0700

    mmoved back to false

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ export let config = {
     users: logins,
     maxAge: 60 * 24 * 31, // 1 month
     //maxage is in minutes, 60 is 1 hour, 60 * 24 is a day and 60 * 24 * 365 is a year
-    signup: true,
+    signup: false,
     signuppath: '/signup',
     maxsignins: 2,
     signintimeout: 300 * 1000 // 5 mins
